3d50decc413fb51d2c8b02bb41648d6cacc08953
Karso2023 <karsoo2023@gmail.com>
2026-02-04 13:14:26 +0000

    testing

--- lib/server.ts
+++ lib/server.ts
@@ -1,6 +1,7 @@
 import { createServerClient } from "@supabase/ssr"
 import { cookies } from "next/headers"
 
+// server side supabase
 export async function createClient() {
   const cookieStore = await cookies()
 
